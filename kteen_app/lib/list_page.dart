6c4aad3f876c2642a369e179defa41c8e3839be3
Yeongbeom Song <gnyf0915@gmail.com>
2021-08-30 02:50:34 +0900

    Feat: Add article search

--- kteen_app/lib/list_page.dart
+++ kteen_app/lib/list_page.dart
@@ -9,11 +9,20 @@ class ListPage extends StatefulWidget {
 
 class _ListPageState extends State<ListPage> {
   Future<List<YouthWelfareService>> _youthWelfareServiceFuture;
+  List<YouthWelfareService> _youthWelfareServiceList = [];
+  List<String> _youthWelfareServiceTitleList = [];
 
   @override
   void initState() {
     super.initState();
-    _youthWelfareServiceFuture = readYouthWelfareService();
+    _youthWelfareServiceFuture = readYouthWelfareService().then((value) {
+      value.forEach((element) {
+        _youthWelfareServiceList.add(element);
+        _youthWelfareServiceTitleList.add(element.TITLE);
+      });
+
+      return value;
+    });
   }
 
   @override
@@ -35,7 +44,7 @@ class _ListPageState extends State<ListPage> {
         IconButton(
           icon: Icon(Icons.search),
           onPressed: () {
-
+            showSearch(context: context, delegate:Search(_youthWelfareServiceList, _youthWelfareServiceTitleList));
           },
         ),
       ],
@@ -90,6 +99,72 @@ class _ListPageState extends State<ListPage> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class Search extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      IconButton(
+        icon: Icon(Icons.close),
+        onPressed: () {
+          query = "";
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  String selectedResult;
+  int selectedResultIndex;
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return ListArticlePage(data: dataList[selectedResultIndex]);
+  }
+
+  final List<YouthWelfareService> dataList;
+  final List<String> searchList;
+
+  Search(this.dataList, this.searchList);
+
+  List<String> recentList = [];
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestionList = [];
+
+    query.isEmpty ? suggestionList = recentList : suggestionList.addAll(
+        searchList.where((element) => element.contains(query),)
+    );
+
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(
+            suggestionList[index],
+          ),
+          leading: query.isEmpty ? Icon(Icons.access_time) : SizedBox(),
+          onTap: () {
+            selectedResult = suggestionList[index];
+            selectedResultIndex = searchList.indexOf(selectedResult);
+
+            showResults(context);
+          },
+        );
+      },
     );
   }
 }
